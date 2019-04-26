76598b06eab2b39171e9fdc47566bf79787450d9
rizalazky <rizalazky26@gmail.com>
2019-04-27 01:59:57 +0700

    menambah fungsi login

---
+++ midlewares/auth.js
@@ -0,0 +1,10 @@
+var auth={
+    check_login:function(req,res,next){
+        if(!req.session.logged_in){
+            return res.redirect('/login');
+        }
+        next();
+    }
+}
+
+module.exports=auth;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var model=require('../models/user');
+var Auth_mid=require('../midlewares/auth');
+
+var sessionStore;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',Auth_mid.check_login, function(req, res, next) {
+  sessionStore=req.session;
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login',function(req,res,next){
+  res.render('login');
+});
+router.post('/login',function(req,res,next){
+  sessionStore=req.session;
+  var email=req.param('email');
+  var password=req.param('password');
+
+  if(email=="" || password==""){
+    req.flash('msg_err','Maaf,Data tidak boleh kosong');
+    res.redirect('/login');
+  }else{
+    model.find({email:email,password:password},function(err,user){
+      if(err){
+        throw err;
+      }
+
+      if(user.length>0){
+        sessionStore.email=user[0].email;
+        sessionStore.username=user[0].username;
+        sessionStore.logged_in=true;
+
+        req.flash('msg_info','Selamat Datang '+user[0].username);
+        res.redirect('users');
+      }else{
+        req.flash('msg_err','Maaf,Email dan password Salah');
+        res.redirect('/login');
+      }
+    });
+  }
+});
 module.exports = router;
